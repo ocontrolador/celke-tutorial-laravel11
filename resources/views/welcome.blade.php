d81de9acd4cf3a8cbded3dddf1b3f12a05b6b3a2
ATCO Dias <diaslasd@gmail.com>
2024-10-16 16:29:23 -0300

    🚀 first commit develop

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" type="image/png" href="favicon.png">
+        <title>Celke</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles / Scripts -->
+        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @else
+            <style></style>
+        @endif
+    </head>
+    <body>
+      <h1> Bem-vindo ao Laravel 11 </h1>
+
+      <p> Data Atual: {{ \Carbon\Carbon::now()->format('d/m/Y H:i:s') }} </p>
+    </body>
+</html>
