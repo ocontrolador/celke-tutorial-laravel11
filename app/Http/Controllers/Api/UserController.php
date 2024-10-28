e71717aeb419b0a1941908f9dd94748b099507ad
ATCO Dias <diaslasd@gmail.com>
2024-10-28 10:12:50 -0300

    🚀 Response com paginação

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,14 +4,23 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {    
-    public function index()
+    /**
+     * Retorna todos os usuários em ordem decrescente por id
+     * e paginado por 2 registros no formato JSON
+     *
+     * @return JsonResponse
+     */
+    public function index() : JsonResponse
     {
+        // recuperar os usuários em ordem decrescente por id e paginar por 2 registros
         $users = User::orderBy('id', 'desc')->paginate(2);
         
+        // retornar os usuários em formato JSON
         return response()->json([
             'status' => true,
             'users' => $users,
