6c68bb320b37908ec9ad5314b9a932a73d2bff46
ATCO Dias <diaslasd@gmail.com>
2024-10-29 01:32:18 -0300

    ✨ Implementado get show user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,26 +5,41 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class UserController extends Controller
-{    
+{
     /**
      * Retorna todos os usuários em ordem decrescente por id
      * e paginado por 2 registros no formato JSON
      *
      * @return JsonResponse
      */
-    public function index() : JsonResponse
+    public function index(): JsonResponse
     {
         // recuperar os usuários em ordem decrescente por id e paginar por 2 registros
         $users = User::orderBy('id', 'desc')->paginate(2);
-        
+
         // retornar os usuários em formato JSON
         return response()->json([
             'status' => true,
             'users' => $users,
             'message' => $users->count() . ' Usuários listados com sucesso'
+        ], 200);
+    }
+
+    /**
+     * Retorna um usuário pelo ID
+     * e em formato JSON
+     *
+     * @param User $user
+     * @return JsonResponse
+     */
+    public function show(User $user): JsonResponse
+    {
+        // retornar o usuário em formato JSON
+        return response()->json([
+            'status' => true,
+            'user' => $user,
         ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/* Route::get('/users', function (Request $request) {
-    return response()->json([
-        'status' => true,
-        'mensagem' => 'Usuário listado com sucesso'
-    ], 200);
-}); */
 
 // get users - http://localhost:8000/api/users?page=2
 Route::get('/users', [UserController::class, 'index']);
+// get user - http://localhost:8000/api/users/1
+Route::get('/user/{user}', [UserController::class, 'show']);
